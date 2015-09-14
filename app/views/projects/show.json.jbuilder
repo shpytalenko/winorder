77ec461e4d23135e761fbdddeb29d04ccd3c34a9
paul_mostovoy <shpytalenko@gmail.com>
2015-09-14 19:30:06 +0300

    projects resource added

---
+++ app/views/projects/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @project, :id, :name, :user_id, :data, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Winorder::Application.routes.draw do
+  resources :projects
+  resources :projects
   root "pages#home"
   get "home", to: "pages#home", as: "home"
   get "inside", to: "pages#inside", as: "inside"
